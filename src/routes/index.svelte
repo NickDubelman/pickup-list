e93111377eb9d670e2627bda53a6d87cdec98c5d
Nick Dubelman <ndubelman@gmail.com>
2021-09-18 18:04:59 -0700

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,4 @@
+<h1>Pickup List</h1>
+<p>Add yourself to a list of people who want to play pickup</p>
+
+<a href="/lists">Lists</a>
---
+++ src/routes/lists.svelte
@@ -0,0 +1,4 @@
+<a href="/">Home</a>
+
+<h1>Lists for this week</h1>
+<a href="/list/1">First list</a>
