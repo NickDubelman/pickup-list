69b20e0f03ce067d2c6bed652e4100e3b000cbb2
Nick Dubelman <ndubelman@gmail.com>
2021-09-18 18:30:28 -0700

    basic skeleton of routes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,30 @@
-<h1>Pickup List</h1>
-<p>Add yourself to a list of people who want to play pickup</p>
+<script>
+	import { goto } from '$app/navigation'
 
-<a href="/lists">Lists</a>
+	function handleSubmit() {
+		// TODO: check if the password is correct
+
+		goto('/lists')
+	}
+</script>
+
+<div>
+	<h1>🏀 🏀 🏀 Pickup List 🏀 🏀 🏀</h1>
+	<p>Add yourself to a list of people who want to play pickup...</p>
+
+	<form on:submit|preventDefault={handleSubmit}>
+		<p>Enter the secret password</p>
+		<input type="text" />
+
+		<button type="submit">Go</button>
+	</form>
+</div>
+
+<style>
+	div {
+		border: 1px solid purple;
+		padding: 0 24px 24px 24px;
+		margin: auto;
+		width: 420px;
+	}
+</style>
--- src/routes/lists.svelte
+++
@@ -1,4 +0,0 @@
-<a href="/">Home</a>
-
-<h1>Lists for this week</h1>
-<a href="/list/1">First list</a>
